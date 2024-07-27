321336d8b9468ee23e080632412efdec0e36253c
Almendras2024 <pittietoon@gmail.com>
2024-07-26 20:51:34 -0400

    imc.py

--- imc.py
+++ imc.py
@@ -1,37 +1,38 @@
 # Define una función para calcular el IMC (Índice de Masa Corporal)
-def calcular_bmi(peso, altura_cm):
+def calcular_imc(peso, altura_cm):
 
 # Convertir la altura de centímetros a metros
     altura_m = altura_cm / 100
     
 # Calcular el IMC
-    bmi = peso / (altura_m ** 2)
-    return bmi
+    imc = peso / (altura_m ** 2)
+    return imc
 
-# Define una función para clasificar el IMC según los estándares de la OMS
-def clasificar_bmi(bmi):
-    if bmi < 18.5:
+# Define una función para clasificar el IMC según la OMS
+def clasificar_imc(imc):
+    if imc < 18.5:
         return "Bajo Peso"
-    elif 18.5 <= bmi < 25:
+    elif 18.5 <= imc < 25:
         return "Adecuado"
-    elif 25 <= bmi < 30:
+    elif 25 <= imc < 30:
         return "Sobrepeso"
-    elif 30 <= bmi < 35:
+    elif 30 <= imc < 35:
         return "Obesidad Grado I"
-    elif 35 <= bmi < 40:
+    elif 35 <= imc < 40:
         return "Obesidad Grado II"
     else:
         return "Obesidad Grado III"
     
 # Ingresar el peso y la altura
 peso = float(input("Ingrese su peso en Kg: \n"))
-altura_cm = float(input("Ingrese su talla (altura) en centímetros: \n"))
+altura_cm = float(input("Ingrese su altura en centímetros: \n"))
 
 # Calcular el IMC
-bmi = calcular_bmi(peso, altura_cm)
+bmi = calcular_imc(peso, altura_cm)
 
 # Clasificar el IMC
-clasificacion = clasificar_bmi(bmi)
+clasificacion = clasificar_imc(imc)
 
 # Imprimir los resultados
-print(f"Su IMC es: {bmi:.2f}, lo que indica una clasificación de: {clasificacion}.")
+print(f"Su IMC es: {imc:.2f}, lo que indica que su clasificación dada por la OMS es
+: {clasificacion}.")
